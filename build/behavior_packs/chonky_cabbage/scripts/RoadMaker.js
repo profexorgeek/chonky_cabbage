464bdf10b71215ae7c72327a07d50101c49aeb8a
Justin Johnson <justin.d.johnson@gmail.com>
2024-02-15 11:59:00 -0700

    System now renders road, bridge, and tunnels

--- build/behavior_packs/chonky_cabbage/scripts/RoadMaker.js
+++ build/behavior_packs/chonky_cabbage/scripts/RoadMaker.js
@@ -2,13 +2,154 @@ import { world, BlockPermutation } from "@minecraft/server";
 import Debug from "./Debug";
 export default class RoadMaker {
     constructor() {
+        this.Dimension = "overworld";
+        this.Air = BlockPermutation.resolve("minecraft:air");
         this.Foundation = BlockPermutation.resolve("minecraft:cobblestone");
         this.Wall = BlockPermutation.resolve("minecraft:cobblestone_wall");
         this.Path = BlockPermutation.resolve("minecraft:bamboo_planks");
         this.Post = BlockPermutation.resolve("minecraft:bamboo_fence");
         this.Light = BlockPermutation.resolve("minecraft:lantern");
+        this.blockInts = [
+            this.Air,
+            this.Foundation,
+            this.Wall,
+            this.Path,
+            this.Post,
+            this.Light //5
+        ];
+        this.RoadNormal = [
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0,
+            2, 0, 0, 0, 2,
+            1, 3, 3, 3, 1
+        ];
+        this.RoadNormalLit = [
+            5, 0, 0, 0, 5,
+            4, 0, 0, 0, 4,
+            4, 0, 0, 0, 4,
+            1, 0, 0, 0, 1,
+            1, 3, 3, 3, 1
+        ];
+        this.RoadTunnel = [
+            1, 3, 3, 3, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 3, 3, 3, 1
+        ];
+        this.RoadTunnelLit = [
+            1, 3, 3, 3, 1,
+            1, 0, 0, 0, 1,
+            5, 0, 0, 0, 5,
+            1, 0, 0, 0, 1,
+            1, 3, 3, 3, 1
+        ];
     }
-    // gets a cardinally-aligned value expressed as an integer where 0 = East, 1 = North, etc
+    renderSlice(sliceTemplate, coord, cardinalDirection) {
+        const rowOffset = 4;
+        const colOffset = -2;
+        Debug.debug(`Rendering slice at ${Debug.printCoordinate3(coord)}`);
+        // loop through each row in the slice template,
+        // we start at 5 above the starting coord and loop
+        // in reverse so we render the slice from the top
+        // left
+        for (let row = 0; row < 5; row++) {
+            // loop through each column in this row,
+            // we start at -2 so that we start rendering
+            // blocks to the left of the starting coord
+            // and center the slice at the bottom center block
+            for (let col = 0; col < 5; col++) {
+                // get the block int from the slice template
+                var blockIndex = sliceTemplate[(row * 5) + col];
+                // resolve the block type based on the int
+                var block = this.blockInts[blockIndex];
+                // transform our slice based on the cardinal direction
+                switch (cardinalDirection) {
+                    case 0: // west
+                        this.setBlock(block, {
+                            x: coord.x,
+                            y: coord.y + (rowOffset - row),
+                            z: coord.z + (colOffset + col)
+                        });
+                        break;
+                    case 1: // south
+                        this.setBlock(block, {
+                            x: coord.x + (colOffset + col),
+                            y: coord.y + (rowOffset - row),
+                            z: coord.z
+                        });
+                        break;
+                    case 2: // east
+                        this.setBlock(block, {
+                            x: coord.x,
+                            y: coord.y + (rowOffset - row),
+                            z: coord.z - (colOffset + col)
+                        });
+                        break;
+                    case 3: // north
+                        this.setBlock(block, {
+                            x: coord.x - (colOffset + col),
+                            y: coord.y + (rowOffset - row),
+                            z: coord.z
+                        });
+                        break;
+                }
+            }
+        }
+    }
+    createRoad(startCoord, viewDirection, length) {
+        Debug.debug(`Starting ${length} road at ${Debug.printCoordinate3(startCoord)}.`);
+        // round our look angle to a cardinal direction, expressed as an integer where 0 = East
+        const cardinalInt = RoadMaker.getCardinalInteger(viewDirection);
+        Debug.debug(`Our cardinal integer is: ${cardinalInt}`);
+        // copy our provided vector into an object we can walk through
+        let walkingCoord = {
+            x: startCoord.x,
+            y: startCoord.y,
+            z: startCoord.z
+        };
+        // loop through our length, creating strips of road depending on our cardinal alignment
+        // we are 1 indexed so the road starts just in front of the player, otherwise it
+        // won't render correctly
+        for (let i = 1; i <= length; i++) {
+            let drawLights = i % 8 === 0;
+            let drawSupports = i % 16 === 0;
+            // dynamically set our length-walking coordinate
+            let coordToChange = cardinalInt % 2 == 0 ? "x" : "z";
+            let directionModifier = cardinalInt > 1 ? -1 : 1;
+            walkingCoord[coordToChange] = startCoord[coordToChange] + (i * directionModifier);
+            // resolve our slice template
+            let sliceTemplate = drawLights ? this.RoadNormalLit : this.RoadNormal;
+            // figure out if we should be tunneling
+            if (this.getBlock({ x: walkingCoord.x, y: walkingCoord.y + 4, z: walkingCoord.z })?.permutation !== this.Air) {
+                sliceTemplate = drawLights ? this.RoadTunnelLit : this.RoadTunnel;
+            }
+            // figure out if we are floating and need supports
+            let supportY = { x: walkingCoord.x, y: walkingCoord.y - 1, z: walkingCoord.z };
+            while (drawSupports && this.getBlock(supportY)?.permutation === this.Air) {
+                this.setBlock(this.Foundation, supportY);
+                supportY.y -= 1;
+            }
+            // render our road
+            this.renderSlice(sliceTemplate, walkingCoord, cardinalInt);
+        }
+    }
+    // sets the block at the provided coordinates to the provided permutation
+    setBlock(blockPerm, coord) {
+        Debug.trace(`Setting block at ${Debug.printCoordinate3(coord)}.`);
+        this.getBlock(coord)?.setPermutation(blockPerm);
+    }
+    // gets the block at the provided coordinates
+    getBlock(coord) {
+        const overworld = world.getDimension(this.Dimension);
+        return overworld.getBlock(coord);
+    }
+    // gets a cardinally-aligned value expressed as an integer where
+    // 0 = West
+    // 1 = South
+    // 2 = East
+    // 3 = North
     static getCardinalInteger(viewDirection) {
         // figure out our 2d look angle
         let lookAngle2d = Math.atan2(viewDirection.z, viewDirection.x);
@@ -21,75 +162,6 @@ export default class RoadMaker {
         // clamp our look angle to 0 - 3
         cardinal = cardinal > 3 ? 0 : cardinal;
         return cardinal;
-    }
-    createRoad(startCoord, viewDirection, length) {
-        Debug.debug(`Starting ${length} road at ${Debug.printCoordinate3(startCoord)}.`);
-        // round our look angle to a cardinal direction, expressed as an integer where 0 = East
-        const cardinalInt = RoadMaker.getCardinalInteger(viewDirection);
-        Debug.debug(`Our cardinal integer is: ${cardinalInt}`);
-        // loop through our length, creating strips of road depending on our cardinal alignment
-        for (let i = 0; i < length; i++) {
-            let drawLights = i % 8 === 0;
-            let drawSupports = i % 16 === 0;
-            switch (cardinalInt) {
-                case 0:
-                    this.makeXAxisStrip({ x: startCoord.x + i, y: startCoord.y, z: startCoord.z }, drawLights, drawSupports);
-                    break;
-                case 1:
-                    this.makeZAxisStrip({ x: startCoord.x, y: startCoord.y, z: startCoord.z + i }, drawLights, drawSupports);
-                    break;
-                case 2:
-                    this.makeXAxisStrip({ x: startCoord.x - i, y: startCoord.y, z: startCoord.z }, drawLights, drawSupports);
-                    break;
-                case 3:
-                    this.makeZAxisStrip({ x: startCoord.x, y: startCoord.y, z: startCoord.z - i }, drawLights, drawSupports);
-                    break;
-            }
-        }
-    }
-    // creates a strip of road aligned with the X axis
-    makeXAxisStrip(center, drawLights = false, drawSupportst = false) {
-        // foundation
-        this.setBlock(this.Foundation, { x: center.x, y: center.y, z: center.z + 2 });
-        this.setBlock(this.Path, { x: center.x, y: center.y, z: center.z + 1 });
-        this.setBlock(this.Path, { x: center.x, y: center.y, z: center.z });
-        this.setBlock(this.Path, { x: center.x, y: center.y, z: center.z - 1 });
-        this.setBlock(this.Foundation, { x: center.x, y: center.y, z: center.z - 2 });
-        // walls
-        this.setBlock(this.Wall, { x: center.x, y: center.y + 1, z: center.z + 2 });
-        this.setBlock(this.Wall, { x: center.x, y: center.y + 1, z: center.z - 2 });
-        if (drawLights === true) {
-            this.makeLamp({ x: center.x, y: center.y, z: center.z + 2 });
-            this.makeLamp({ x: center.x, y: center.y, z: center.z - 2 });
-        }
-    }
-    // creates a strip of road aligned with the Z axis
-    makeZAxisStrip(center, drawLights = false, drawSupportst = false) {
-        // foundation
-        this.setBlock(this.Foundation, { x: center.x - 2, y: center.y, z: center.z });
-        this.setBlock(this.Path, { x: center.x - 1, y: center.y, z: center.z });
-        this.setBlock(this.Path, { x: center.x, y: center.y, z: center.z });
-        this.setBlock(this.Path, { x: center.x + 1, y: center.y, z: center.z });
-        this.setBlock(this.Foundation, { x: center.x + 2, y: center.y, z: center.z });
-        // walls
-        this.setBlock(this.Wall, { x: center.x + 2, y: center.y + 1, z: center.z });
-        this.setBlock(this.Wall, { x: center.x - 2, y: center.y + 1, z: center.z });
-        if (drawLights === true) {
-            this.makeLamp({ x: center.x + 2, y: center.y, z: center.z });
-            this.makeLamp({ x: center.x - 2, y: center.y, z: center.z });
-        }
-    }
-    makeLamp(center) {
-        this.setBlock(this.Foundation, { x: center.x, y: center.y + 1, z: center.z });
-        this.setBlock(this.Post, { x: center.x, y: center.y + 2, z: center.z });
-        this.setBlock(this.Post, { x: center.x, y: center.y + 3, z: center.z });
-        this.setBlock(this.Light, { x: center.x, y: center.y + 4, z: center.z });
-    }
-    // sets the block at the provided coordinates to the provided permutation
-    setBlock(blockPerm, coord) {
-        Debug.trace(`Setting block at ${Debug.printCoordinate3(coord)}.`);
-        const overworld = world.getDimension("overworld");
-        overworld.getBlock(coord)?.setPermutation(blockPerm);
     }
 }
 
--- scripts/RoadMaker.ts
+++ scripts/RoadMaker.ts
@@ -1,15 +1,208 @@
 import {world, BlockPermutation, Vector3, Vector2 } from "@minecraft/server";
 import Debug from "./Debug";
 
+// For block definitions, see:
+// https://www.npmjs.com/package/@minecraft/vanilla-data?activeTab=code
 export default class RoadMaker {
 
+  // these fields are used to keep track
+  // of where we are in the current work
+  // queue, allowing us to do long-running
+  // work across multiple ticks
+  private queueStartCoord: Vector3;
+  private queueViewDirection: Vector3;
+  private queueCurrentIteration: number;
+  private queueRoadLength: number;
+  private queueCardinalDirection: number;
+
+  // these properties allow implementations to specify the specific
+  // road style
+  public Dimension =   "overworld";
+  public Air =         BlockPermutation.resolve("minecraft:air");
+  public Dirt =        BlockPermutation.resolve("minecraft:dirt");
   public Foundation =  BlockPermutation.resolve("minecraft:cobblestone");
   public Wall =        BlockPermutation.resolve("minecraft:cobblestone_wall");
   public Path =        BlockPermutation.resolve("minecraft:bamboo_planks");
+  public Stair =       BlockPermutation.resolve("minecraft:bamboo_stair");
   public Post =        BlockPermutation.resolve("minecraft:bamboo_fence");
   public Light =       BlockPermutation.resolve("minecraft:lantern");
 
-  // gets a cardinally-aligned value expressed as an integer where 0 = East, 1 = North, etc
+  // this maps block types to an integer index, allowing easy
+  // creation of visual templates
+  private blockInts:Array<BlockPermutation> =[
+    this.Air,         //0
+    this.Foundation,  //1
+    this.Wall,        //2
+    this.Path,        //3
+    this.Post,        //4
+    this.Light        //5
+  ]
+
+  // these are template 5x5 "slices" of a type of road. These use the
+  // int list above to resolve blocks when rendering the slice
+  private RoadNormal:Array<number> = [
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0,
+    2,0,0,0,2,
+    1,3,3,3,1];
+
+  private RoadNormalLit:Array<number> = [
+    5,0,0,0,5,
+    4,0,0,0,4,
+    4,0,0,0,4,
+    1,0,0,0,1,
+    1,3,3,3,1];
+  
+  private RoadTunnel:Array<number> = [
+    1,3,3,3,1,
+    1,0,0,0,1,
+    1,0,0,0,1,
+    1,0,0,0,1,
+    1,3,3,3,1];
+
+  private RoadTunnelLit:Array<number> = [
+    1,3,3,3,1,
+    1,0,0,0,1,
+    5,0,0,0,5,
+    1,0,0,0,1,
+    1,3,3,3,1];
+
+  // renders a single slice of road using the provided template. It uses the
+  // coordinate as the bottom center of the template slice and transforms
+  // based on the cardinal direction
+  renderSlice(sliceTemplate: Array<number>, coord:Vector3, cardinalDirection: number)
+  {
+    const rowOffset = 4;
+    const colOffset = -2;
+
+    Debug.debug(`Rendering slice at ${Debug.printCoordinate3(coord)}`);
+
+    // loop through each row in the slice template,
+    // we start at 5 above the starting coord and loop
+    // in reverse so we render the slice from the top
+    // left
+    for(let row = 0; row < 5; row++)
+    {
+      // loop through each column in this row,
+      // we start at -2 so that we start rendering
+      // blocks to the left of the starting coord
+      // and center the slice at the bottom center block
+      for(let col = 0; col < 5; col++)
+      {
+        // get the block int from the slice template
+        var blockIndex = sliceTemplate[(row * 5) + col];
+
+        // resolve the block type based on the int
+        var block = this.blockInts[blockIndex];
+
+        // transform our slice based on the cardinal direction
+        switch(cardinalDirection)
+        {
+          case 0: // west
+            this.setBlock(block, {
+              x: coord.x,
+              y: coord.y + (rowOffset - row),
+              z: coord.z + (colOffset + col)
+            });
+            break;
+          case 1: // south
+            this.setBlock(block, {
+              x: coord.x + (colOffset + col),
+              y: coord.y + (rowOffset - row),
+              z: coord.z
+            });
+            break;
+          case 2: // east
+            this.setBlock(block, {
+              x: coord.x,
+              y: coord.y + (rowOffset - row),
+              z: coord.z - (colOffset + col)
+            });
+            break;
+          case 3: // north
+            this.setBlock(block, {
+              x: coord.x - (colOffset + col),
+              y: coord.y + (rowOffset - row),
+              z: coord.z
+            });
+            break;
+        }
+      }
+    }
+  }
+
+  
+
+  createRoad(startCoord: Vector3, viewDirection: Vector3, length: number)
+  {
+    Debug.debug(`Starting ${length} road at ${Debug.printCoordinate3(startCoord)}.`)
+
+    // round our look angle to a cardinal direction, expressed as an integer where 0 = East
+    const cardinalInt = RoadMaker.getCardinalInteger(viewDirection);
+
+    Debug.debug(`Our cardinal integer is: ${cardinalInt}`);
+
+    // copy our provided vector into an object we can walk through
+    let walkingCoord = {
+      x: startCoord.x,
+      y: startCoord.y,
+      z: startCoord.z
+    };
+
+    // loop through our length, creating strips of road depending on our cardinal alignment
+    // we are 1 indexed so the road starts just in front of the player, otherwise it
+    // won't render correctly
+    for(let i = 1; i <= length; i++)
+    {
+      let drawLights = i % 8 === 0;
+      let drawSupports = i % 16 === 0;
+
+      // dynamically set our length-walking coordinate
+      let coordToChange: keyof Vector3 = cardinalInt % 2 == 0 ? "x" : "z";
+      let directionModifier = cardinalInt > 1 ? -1 : 1;
+      walkingCoord[coordToChange] = startCoord[coordToChange] + (i * directionModifier);
+
+      // resolve our slice template
+      let sliceTemplate = drawLights ? this.RoadNormalLit : this.RoadNormal;
+
+      // figure out if we should be tunneling
+      if(this.getBlock({x: walkingCoord.x, y: walkingCoord.y + 4, z: walkingCoord.z})?.permutation !== this.Air)
+      {
+        sliceTemplate = drawLights ? this.RoadTunnelLit : this.RoadTunnel;
+      }
+
+      // figure out if we are floating and need supports
+      let supportY = {x: walkingCoord.x, y: walkingCoord.y - 1, z: walkingCoord.z}
+      while(drawSupports && this.getBlock(supportY)?.permutation !== this.Air)
+      {
+        this.setBlock(this.Foundation, supportY);
+        supportY.y -= 1;
+      }
+
+      // render our road
+      this.renderSlice(sliceTemplate, walkingCoord, cardinalInt);
+    }
+  }
+
+  // sets the block at the provided coordinates to the provided permutation
+  setBlock(blockPerm:BlockPermutation, coord:Vector3) {
+    Debug.trace(`Setting block at ${Debug.printCoordinate3(coord)}.`);
+    this.getBlock(coord)?.setPermutation(blockPerm);
+  }
+
+  // gets the block at the provided coordinates
+  getBlock(coord: Vector3)
+  {
+    const overworld = world.getDimension(this.Dimension);
+    return overworld.getBlock(coord)
+  }
+
+  // gets a cardinally-aligned value expressed as an integer where
+  // 0 = West
+  // 1 = South
+  // 2 = East
+  // 3 = North
   static getCardinalInteger(viewDirection:Vector3): number
   {
     // figure out our 2d look angle
@@ -28,94 +221,5 @@ export default class RoadMaker {
     cardinal = cardinal > 3 ? 0 : cardinal;
 
     return cardinal;
-  }
-
-  createRoad(startCoord: Vector3, viewDirection: Vector3, length: number)
-  {
-    Debug.debug(`Starting ${length} road at ${Debug.printCoordinate3(startCoord)}.`)
-
-    // round our look angle to a cardinal direction, expressed as an integer where 0 = East
-    const cardinalInt = RoadMaker.getCardinalInteger(viewDirection);
-
-    Debug.debug(`Our cardinal integer is: ${cardinalInt}`);
-
-    // loop through our length, creating strips of road depending on our cardinal alignment
-    for(let i = 0; i < length; i++)
-    {
-      let drawLights = i % 8 === 0;
-      let drawSupports = i % 16 === 0;
-
-      switch(cardinalInt){
-        case 0:
-          this.makeXAxisStrip({x:startCoord.x + i, y: startCoord.y, z:startCoord.z}, drawLights, drawSupports);
-          break;
-        case 1:
-          this.makeZAxisStrip({x:startCoord.x, y: startCoord.y, z:startCoord.z + i}, drawLights, drawSupports);
-          break;
-        case 2:
-          this.makeXAxisStrip({x:startCoord.x - i, y: startCoord.y, z:startCoord.z}, drawLights, drawSupports);
-          break;
-        case 3:
-          this.makeZAxisStrip({x:startCoord.x, y: startCoord.y, z:startCoord.z - i}, drawLights, drawSupports);
-          break;
-      }
-    }
-  }
-
-  // creates a strip of road aligned with the X axis
-  makeXAxisStrip(center:Vector3, drawLights:boolean = false, drawSupportst:boolean = false)
-  {
-    // foundation
-    this.setBlock(this.Foundation,{x:center.x,y:center.y,z:center.z+2});
-    this.setBlock(this.Path,{x:center.x,y:center.y,z:center.z+1});
-    this.setBlock(this.Path,{x:center.x,y:center.y,z:center.z});
-    this.setBlock(this.Path,{x:center.x,y:center.y,z:center.z-1});
-    this.setBlock(this.Foundation,{x:center.x,y:center.y,z:center.z-2});
-
-    // walls
-    this.setBlock(this.Wall,{x:center.x,y:center.y+1,z:center.z+2});
-    this.setBlock(this.Wall,{x:center.x,y:center.y+1,z:center.z-2});
-
-    if(drawLights === true)
-    {
-      this.makeLamp({x:center.x,y:center.y,z:center.z+2});
-      this.makeLamp({x:center.x,y:center.y,z:center.z-2});
-    }
-  }
-
-  // creates a strip of road aligned with the Z axis
-  makeZAxisStrip(center:Vector3, drawLights:boolean = false, drawSupportst:boolean = false)
-  {
-    // foundation
-    this.setBlock(this.Foundation,{x:center.x-2,y:center.y,z:center.z});
-    this.setBlock(this.Path,{x:center.x-1,y:center.y,z:center.z});
-    this.setBlock(this.Path,{x:center.x,y:center.y,z:center.z});
-    this.setBlock(this.Path,{x:center.x+1,y:center.y,z:center.z});
-    this.setBlock(this.Foundation,{x:center.x+2,y:center.y,z:center.z});
-
-    // walls
-    this.setBlock(this.Wall,{x:center.x+2,y:center.y+1,z:center.z});
-    this.setBlock(this.Wall,{x:center.x-2,y:center.y+1,z:center.z});
-
-    if(drawLights === true)
-    {
-      this.makeLamp({x:center.x+2,y:center.y,z:center.z});
-      this.makeLamp({x:center.x-2,y:center.y,z:center.z});
-    }
-  }
-
-  makeLamp(center:Vector3)
-  {
-    this.setBlock(this.Foundation,{x: center.x,y:center.y+1,z:center.z});
-    this.setBlock(this.Post,{x: center.x,y:center.y+2,z:center.z});
-    this.setBlock(this.Post,{x: center.x,y:center.y+3,z:center.z});
-    this.setBlock(this.Light,{x: center.x,y:center.y+4,z:center.z});
-  }
-
-  // sets the block at the provided coordinates to the provided permutation
-  setBlock(blockPerm:BlockPermutation, coord:Vector3) {
-    Debug.trace(`Setting block at ${Debug.printCoordinate3(coord)}.`);
-    const overworld = world.getDimension("overworld");
-    overworld.getBlock(coord)?.setPermutation(blockPerm);
   }
 }
